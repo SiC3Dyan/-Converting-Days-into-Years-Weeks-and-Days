abd76f2d1ed515f5e8ac40efafbeed5631f36bdd
rcedric_246 <rcedric.cruz@gmail.com>
2026-04-13 23:56:04 +0800

    Update ACT 2.cpp

--- ACT 2.cpp
+++ ACT 2.cpp
@@ -12,5 +12,7 @@ int main () {
   int weeks = daysLeft / 7;
   int remainingDays = 5;
 
-  
+  cout << "Years: " << years << endl;
+  cout << "Weeks: " << weeks << endl; 
+  cout << "Days: " << days << endl;
 }
