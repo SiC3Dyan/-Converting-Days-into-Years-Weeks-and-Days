3f69a291bd0364f34b35357e805fbdf73ce846f0
rcedric_246 <rcedric.cruz@gmail.com>
2026-04-15 23:58:12 +0800

    Update ACT 2.cpp

--- ACT 2.cpp
+++ ACT 2.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int main () {
+  int totalDays;
+  cout << "Enter Number of Days: ";
+  cout >> totalDays;
+
+  int years = totalDays/365;
+  int dayLeft = totalDays - (years*365)
+
+  int weeks = daysLeft/7;
+  int remainingDays = 5;
+
+  cout << "Years: " << years << endl;
+  cout << "Weeks: " << weeks << endl;
+  cout << "Days: " << Days << endl;
+
+  return 0;
+}
